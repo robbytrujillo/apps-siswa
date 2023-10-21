4ef8581882342bb8d48282b00ae7c8de3d868004
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-21 07:07:05 +0700

    Apps-Siswa | Form Tambah Siswa

--- application/views/tambah_siswa.php
+++ application/views/tambah_siswa.php
@@ -1,4 +1,4 @@
-<form action="<?= base_url('siswa/tambah_aksi') ?>">
+<form action="<?= base_url('siswa/tambah_aksi') ?>" method="POST">
     <div class="form-group">
         <label>Nama Siswa</label>
         <input type="text" name="nama_siswa" class="form-control">
@@ -11,7 +11,7 @@
     </div>
     <div class="form-group">
         <label>Alamat Siswa</label>
-        <textarea name="alamat_siswa" cols="30" rows="10"></textarea>
+        <textarea name="alamat_siswa" class="form-control"></textarea>
         <?= form_error('alamat_siswa', '<div class="text-small text-danger">','</div>'); ?>
     </div>
     <div class="form-group">
@@ -19,4 +19,6 @@
         <input type="text" name="nomor_telepon" class="form-control">
         <?= form_error('nomor_telepon', '<div class="text-small text-danger">','</div>'); ?>
     </div>
+    <button type="submit" class="btn btn-primary btn-sm"><i class="fas fa-save"></i>Simpan</button>
+    <button type="reset" class="btn btn-danger btn-sm"><i class="fas fa-trash"></i>Reset</button>
 </form>
